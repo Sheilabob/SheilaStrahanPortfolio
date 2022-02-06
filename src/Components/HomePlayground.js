feb9e2128edfe06e56fe90c7322c650ffd68d25d
Sheila Strahan <smstrahan@gmail.com>
2022-02-05 22:17:02 -0700

    toggled ball animation

--- src/Components/HomePlayground.js
+++ src/Components/HomePlayground.js
@@ -1,35 +1,79 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
 const ballStyle = {
     display: 'block',
     width: '1rem',
     height: '1rem',
-    background: 'black',
+    background: 'green',
     borderRadius: '0.5rem'
 }
 
 const bounceTransition = {
     y: {
-      duration: 0.4,
+      duration: 0.6,
       yoyo: Infinity,
       ease: "easeOut",
-    },
-    backgroundColor: {
-      duration: 0,
-      yoyo: Infinity,
-      ease: "easeOut",
-      repeatDelay: 0.8,
-    },
+    }
   }
 
 export default function HomePlayground() {
+    const [bounce, setBounce] = useState(false);
+
+    const animatedBall = () => {
+      if (bounce) {
+        return (<div
+        style={{
+          width: "4rem",
+          height: "4rem",
+          outlineColor: "black",
+          outlineStyle: "solid",
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "20px",
+          padding: "20px"
+        }}>
+          
+          <motion.span 
+          style={ballStyle}
+          transition={bounce && bounceTransition}
+          animate={bounce && {
+            y: ["300%", "-100%"],
+            backgroundColor: "red",
+          }}
+          />
+          
+      </div>)
+      } else {
+        return (<div
+        style={{
+          width: "4rem",
+          height: "4rem",
+          outlineColor: "black",
+          outlineStyle: "solid",
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "20px",
+          padding: "20px"
+        }}>
+          
+          <motion.span 
+          style={ballStyle}
+          />
+          
+      </div>)
+      }
+    }
+
     return ( 
-        <div>
+        <div >
             <div>
                 <h1>This is where the playground goes.</h1>
             </div>
-       <div
+            <div onClick={() => setBounce(!bounce)}>
+              {animatedBall()}
+            </div>
+       {/* <div onClick={() => setBounce(!bounce)}
       style={{
         width: "4rem",
         height: "4rem",
@@ -40,16 +84,17 @@ export default function HomePlayground() {
         margin: "20px",
         padding: "20px"
       }}>
-    
-        <motion.span
+        
+        <motion.span 
         style={ballStyle}
-        transition={bounceTransition}
-        animate={{
+        transition={bounce && bounceTransition}
+        animate={bounce && {
           y: ["100%", "-100%"],
           backgroundColor: ["green", "red"],
         }}
-      />
-    </div>
+        />
+        
+    </div> */}
     </div>
    )
 }

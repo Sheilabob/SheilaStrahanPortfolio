2cbf026aa9f8b76e4f1d709e55c78020b93f0b16
Sheila Strahan <smstrahan@gmail.com>
2022-02-17 19:59:15 -0700

    added independence and direction to balls

--- src/Components/HomePlayground.js
+++ src/Components/HomePlayground.js
@@ -9,7 +9,6 @@ const ballStyle = {
     height: '20px',
     background: '#ACF6FA',
     borderRadius: '10px',
-    // boxShadow: '-3px 5px lightBlue'
 }
 
 const bounceTransition = {
@@ -30,7 +29,10 @@ const note = new Audio();
 
 export default function HomePlayground() {
     const [bounce, setBounce] = useState(false);
-    const [music, playMusic] = useState(false);
+    const [bounce1, setBounce1] = useState(false);
+    const [bounce2, setBounce2] = useState(false);
+
+
     const [loops, setLoops] = useState(false)
 
 
@@ -42,8 +44,35 @@ export default function HomePlayground() {
      
     }
 
-    const animatedBall = () => {
-      if (bounce) {
+    const animatedBall = (bounceNumber, direction) => {
+      let y1 = "100%";
+      let y2 = "100%";
+      let x1 = "100%";
+      let x2 = "100%";
+      if (direction === "right") {
+         y1 = "-50%";
+         y2 = "-50%";
+         x1 = "-200%";
+         x2 = "100%";
+       } else if (direction === "up") {
+         y1 = "100%";
+         y2 = "-200%";
+         x1 = "-50%";
+         x2 = "-50%";
+        } else if (direction === "diagLeft") {
+          y1 = "100%";
+          y2 = "-200%";
+          x1 = "100%";
+          x2 = "-200%";
+        } else if (direction === "diagRight") {
+          y1 = "100%";
+          y2 = "-200%";
+          x1 = "-200%";
+          x2 = "100%";
+        }
+
+
+      if (bounceNumber) {
         return (<div
         style={{
           width: "4rem",
@@ -56,10 +85,10 @@ export default function HomePlayground() {
           
           <motion.span 
           style={ballStyle}
-          transition={bounce && bounceTransition}
-          animate={bounce && {
-            y: ["100%", "-100%"],
-            x: ["-100%", "100%"],
+          transition={bounceNumber && bounceTransition}
+          animate={bounceNumber && {
+            y: [y1, y2],
+            x: [x1, x2],
             backgroundColor: "#0043C9", 
 
                     }}
@@ -92,27 +121,24 @@ export default function HomePlayground() {
             <Container>
               <Row>
                 <Col className="justify-content-center text-center m-1" style={{outline: 'solid black'}} onClick={() => {
-              setBounce(!bounce);
-              playMusic(!music);
+              setBounce1(!bounce1);
               setLoops(!loops);
             }}>
-              {animatedBall()}
+              {animatedBall(bounce1, "right")}
+              {playNote(loops)}
+                </Col>
+                <Col onClick={() => {
+              setBounce2(!bounce2);
+              setLoops(!loops);
+            }}>
+              {animatedBall(bounce2, "diagLeft")}
               {playNote(loops)}
                 </Col>
                 <Col onClick={() => {
               setBounce(!bounce);
-              playMusic(!music);
               setLoops(!loops);
             }}>
-              {animatedBall()}
-              {playNote(loops)}
-                </Col>
-                <Col onClick={() => {
-              setBounce(!bounce);
-              playMusic(!music);
-              setLoops(!loops);
-            }}>
-              {animatedBall()}
+              {animatedBall(bounce, "diagRight")}
               {playNote(loops)}
                 </Col>
               </Row>

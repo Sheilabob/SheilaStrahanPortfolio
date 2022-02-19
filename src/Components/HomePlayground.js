98823ad3800360315dcd54bb49e843a8699ffc09
Sheila Strahan <smstrahan@gmail.com>
2022-02-18 18:39:56 -0700

    separated sound for balls

--- src/Components/HomePlayground.js
+++ src/Components/HomePlayground.js
@@ -5,10 +5,10 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const ballStyle = {
     display: 'flex',
-    width: '20px',
-    height: '20px',
+    width: '40px',
+    height: '40px',
     background: '#ACF6FA',
-    borderRadius: '10px',
+    borderRadius: '20px',
 }
 
 const bounceTransition = {
@@ -25,7 +25,14 @@ const bounceTransition = {
     }
   }
 
-const note = new Audio();
+const Cnote = new Audio();
+const Dnote = new Audio();
+const Enote = new Audio();
+const Fnote = new Audio();
+const Gnote = new Audio();
+const Anote = new Audio();
+const Bnote = new Audio();
+const highCnote = new Audio()
 
 export default function HomePlayground() {
     const [bounce, setBounce] = useState(false);
@@ -33,16 +40,97 @@ export default function HomePlayground() {
     const [bounce2, setBounce2] = useState(false);
 
 
-    const [loops, setLoops] = useState(false)
+    const [loopsC, setLoopsC] = useState(false);
+    const [loopsD, setLoopsD] = useState(false);
+    const [loopsE, setLoopsE] = useState(false);
+    const [loopsF, setLoopsF] = useState(false);
+    const [loopsG, setLoopsG] = useState(false);
+    const [loopsA, setLoopsA] = useState(false);
+    const [loopsB, setLoopsB] = useState(false);
+    const [loopsHighC, setLoopsHighC] = useState(false);
 
 
-    const playNote = (x) => {
-     if (loops) {
-      note.src = "../assets/audio/Csharp.mp3"} else {note.src = "";};
-      note.play();
-      note.loop = true;
-     
+
+    const playCNote = () => {
+      if (loopsC == true) {
+        Cnote.src = "../assets/audio/C.mp3";
+      } else {
+        Cnote.src = ""
+      };
+      Cnote.play();
+      Cnote.loop = true;
     }
+
+    const playDNote = () => {
+      if (loopsD == true) {
+        Dnote.src = "../assets/audio/D.mp3";
+      } else {
+        Dnote.src = ""
+      };
+      Dnote.play();
+      Dnote.loop = true;
+    }
+
+    const playENote = () => {
+      if (loopsE == true) {
+        Enote.src = "../assets/audio/E.mp3";
+      } else {
+        Enote.src = ""
+      };
+      Enote.play();
+      Enote.loop = true;
+    }
+
+    const playFNote = () => {
+      if (loopsF == true) {
+        Fnote.src = "../assets/audio/F.mp3";
+      } else {
+        Fnote.src = ""
+      };
+      Fnote.play();
+      Fnote.loop = true;
+    }
+
+    const playGNote = () => {
+      if (loopsG == true) {
+        Gnote.src = "../assets/audio/G.mp3";
+      } else {
+        Gnote.src = ""
+      };
+      Gnote.play();
+      Gnote.loop = true;
+    }
+
+    const playANote = () => {
+      if (loopsA == true) {
+        Anote.src = "../assets/audio/A.mp3";
+      } else {
+        Anote.src = ""
+      };
+      Anote.play();
+      Anote.loop = true;
+    }
+
+    const playBNote = () => {
+      if (loopsB == true) {
+        Bnote.src = "../assets/audio/B.mp3";
+      } else {
+        Bnote.src = ""
+      };
+      Bnote.play();
+      Bnote.loop = true;
+    }
+
+    const playHighCNote = () => {
+      if (loopsHighC == true) {
+        highCnote.src = "../assets/audio/HighC.mp3";
+      } else {
+        highCnote.src = ""
+      };
+      highCnote.play();
+      highCnote.loop = true;
+    }
+
 
     const animatedBall = (bounceNumber, direction) => {
       let y1 = "100%";
@@ -53,22 +141,16 @@ export default function HomePlayground() {
          y1 = "-50%";
          y2 = "-50%";
          x1 = "-200%";
-         x2 = "100%";
        } else if (direction === "up") {
-         y1 = "100%";
          y2 = "-200%";
          x1 = "-50%";
          x2 = "-50%";
         } else if (direction === "diagLeft") {
-          y1 = "100%";
           y2 = "-200%";
-          x1 = "100%";
           x2 = "-200%";
         } else if (direction === "diagRight") {
-          y1 = "100%";
           y2 = "-200%";
           x1 = "-200%";
-          x2 = "100%";
         }
 
 
@@ -122,24 +204,24 @@ export default function HomePlayground() {
               <Row>
                 <Col className="justify-content-center text-center m-1" style={{outline: 'solid black'}} onClick={() => {
               setBounce1(!bounce1);
-              setLoops(!loops);
+              setLoopsC(!loopsC);
             }}>
               {animatedBall(bounce1, "right")}
-              {playNote(loops)}
+              {playCNote()}
                 </Col>
                 <Col onClick={() => {
               setBounce2(!bounce2);
-              setLoops(!loops);
+              setLoopsD(!loopsD);
             }}>
               {animatedBall(bounce2, "diagLeft")}
-              {playNote(loops)}
+              {playDNote()}
                 </Col>
                 <Col onClick={() => {
               setBounce(!bounce);
-              setLoops(!loops);
+              setLoopsE(!loopsE);
             }}>
               {animatedBall(bounce, "diagRight")}
-              {playNote(loops)}
+              {playENote()}
                 </Col>
               </Row>
             <ProfilePic src="../assets/images/MsSheilaRingArch.jpg" />

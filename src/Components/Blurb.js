d536251634f9eb8704747211144854173d202ff3
Sheila Strahan <smstrahan@gmail.com>
2022-03-20 19:12:41 -0600

    new build file

--- src/Components/Blurb.js
+++ src/Components/Blurb.js
@@ -6,7 +6,7 @@ export default function Blurb() {
     return (
         <Container>
             <Row className="m-3">
-               <h6>Welcome! I'm a music teacher turned web developer from Moab, UT, with an interest in all things space and sound, along with a love of the outdoors.  This page was created in React with a lot of vanilla javascript to make all the bells and whistles work.  Check out my other projects below!</h6>
+               <h6>Welcome! I'm a music teacher turned web developer from Moab, UT, with an interest in all things space and sound, along with a love of the outdoors.  This page was created in React with a lot of vanilla javascript to make all the bells and whistles work. (Link to the github code is: <a href='https://github.com/Sheilabob/SheilaStrahanPortfolio'>here</a>.)  Check out my other projects below!</h6>
             </Row>
             </Container>
     )
